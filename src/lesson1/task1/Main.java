82074b45de9857c1a3d25b7aad53dc5503f6a177
f.a.buslaev <f.a.buslaev@tinkoff.ru>
2018-11-07 12:20:07 +0300

    Понял- работает уначе- удаляет шлак ненужный

---
+++ src/lesson1/task1/Main.java
@@ -0,0 +1,16 @@
+package lesson1.task1;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // объявляю переменную
+        String msg = "Hello" + " " + "Bob!";
+        // вывожу строку в консоль
+        System.out.println(msg);
+    }
+}
+
+// Скомпилируйте данный класс и далее выполните декомпиляцию для созданного во время компиляции "Main.class" файла.
+// Попробуйте объяснить, почему содержимое java и class файла разное ?
+
+// Ответ : pft,bc
