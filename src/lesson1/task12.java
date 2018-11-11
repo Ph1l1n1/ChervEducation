189a3cf31ff3e5c51062ebb403c00aa5bb00f4ba
Philip <ph1l1n1n@yandex.ru>
2018-11-11 17:01:27 +0300

    Коммит - Очистит то, что другим не под силу

--- src/lesson1/task12.java
+++ src/lesson1/task12.java
@@ -1,35 +1,55 @@
 package lesson1;
 
+
+import java.util.ArrayList;
+
 /**
  * Напишите метод который принимает математическое выражение в формате String и возвращает результат.
  * В выражении используются только целые числа, поддерживаемые операции:+,-.
  * Пример:
- * 	math("2-1"); // вернёт [1]
- * 	math("2- +1"); // вернёт [1]
- * 	math(" 2-  -1"); // вернёт [3]
+ * math("2-1"); // вернёт [1]
+ * math("2- +1"); // вернёт [1]
+ * math(" 2-  -1"); // вернёт [3]
  */
 
 public class task12 {
 
     public static void main(String[] args) {
 
-        String[] array = {"2"};
+        String[] array = {"2-1", "2- +1", " 2-  -1"};
 
         for (int i = 0; i < array.length; i++) {
-            int is = getboolean(array[i]);
-            System.out.println(is);
+            int num = getnum(array[i]);
+
+
+            System.out.println(num);
         }
-
-
-
-
     }
 
-    private static int getboolean(String s) {
-        Integer  is = null;
+    private static int getnum(String string) {
+        int result = 0;
+        String numbers = "0123456789";
+        String operations = "+-/*";
 
-        is = Integer.valueOf(s);
 
-        return is;
+        for (int i = 0; i < string.length(); i++) {
+            String symbol = String.valueOf(string.charAt(i));
+            if (!symbol.equals(" ")) {
+
+                if (numbers.contains(string.charAt(i) + "")) {
+                    result = result * 10 + (Integer.parseInt(string.charAt(i) + ""));
+                } else {
+                    if (string.charAt(i) == '+') {
+                        result += getnum(string.substring(i + 1));
+                    }
+                    if (string.charAt(i) == '-') {
+                        result -= getnum(string.substring(i + 1));
+                    }
+                    break;
+                }
+
+            }
+        }
+        return result;
     }
 }
