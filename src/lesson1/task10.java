656d9223f08eeaa3f6d73b0b162ed32b7bddce3e
Philip <ph1l1n1n@yandex.ru>
2018-11-11 15:12:48 +0300

    Коммит - Очистит то, что другим не под силу

--- src/lesson1/task10.java
+++ src/lesson1/task10.java
@@ -3,10 +3,25 @@ package lesson1;
 /**
  * Напишите метод который бы принимал параметр String - номер телефона 10 символов (от 0 до 9) и возвращал отформатированный номер.
  * Пример:
- * 	format("9136758070"); //вернёт "+7 (913) 675-80-70"
+ * format("9136758070"); //вернёт "+7 (913) 675-80-70"
  */
 
 
-
 public class task10 {
+    public static void main(String[] args) {
+        String number = "9136758070";
+
+        number = formats(number);
+
+        System.out.println(number);
+
+    }
+
+    private static String formats(String number) {
+
+        number = String.valueOf(number).replaceFirst("(\\d{3})(\\d{3})(\\d+)", "+7 ($1)-$2-$3");
+
+        return number;
+    }
+
 }
