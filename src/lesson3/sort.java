b5bfd127038a72726f67ac53c7dc25e89224ec20
f.a.buslaev <f.a.buslaev@tinkoff.ru>
2018-11-19 13:15:22 +0300

    Коммит - Очистит то, что другим не под силу

--- src/lesson3/sort.java
+++ src/lesson3/sort.java
@@ -1,5 +1,7 @@
 package lesson3;
 
+import java.util.Arrays;
+
 /**
  * Напишите метод sortCards() который сортирует перетасованный список карт, так что любой список карт сортируется по рангу,
  * независимо от начальной коллекции.
@@ -17,21 +19,64 @@ public class sort {
         Character[] array = {'3', '9', 'A', '5', 'T', '8', '2', '4', 'Q', '7', 'J', '6', 'K'};
 
         sortCards(array);
-
     }
 
     private static void sortCards(Character[] array) {
+        String[] newarraystr = new String[13];
 
-        for (int i = 0; i < array.length; i++) {
+        int[] newarray = chartoint(array);
 
-
-
-
-
-
+        for (int i = 0; i < newarray.length; i++) {
+            if ((newarray[i]>1) & (newarray[i]<10) ) {
+                newarraystr[i] = String.valueOf(newarray[i]);
+            } else {
+                if (newarray[i]==1) {
+                    newarraystr[i] = "A";
+                }
+                if (newarray[i]==10) {
+                    newarraystr[i] = "T";
+                }
+                if (newarray[i]==11) {
+                    newarraystr[i] = "J";
+                }
+                if (newarray[i]==12) {
+                    newarraystr[i] = "Q";
+                }
+                if (newarray[i]==13) {
+                    newarraystr[i] = "K";
+                }
+            }
         }
 
+        System.out.println(Arrays.toString(newarraystr));
+    }
 
+    private static int[] chartoint(Character[] array) {
+        int[] newarray = new int[13];
+
+        for (int i = 0; i < array.length; i++) {
+            if ((Character.isDigit(array[i]))) {
+                newarray[i] = Integer.valueOf(array[i].toString());
+            } else {
+                if (array[i].toString().equals("A")) {
+                    newarray[i] = 1;
+                }
+                if (array[i].toString().equals("T")) {
+                    newarray[i] = 10;
+                }
+                if (array[i].toString().equals("J")) {
+                    newarray[i] = 11;
+                }
+                if (array[i].toString().equals("Q")) {
+                    newarray[i] = 12;
+                }
+                if (array[i].toString().equals("K")) {
+                    newarray[i] = 13;
+                }
+            }
+        }
+        Arrays.sort(newarray);
+        return newarray;
     }
 
 
